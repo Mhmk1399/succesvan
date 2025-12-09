dad76ccc88d469a0392e885ba6bbf53609be323e
Hossein <141348426+Hossein137900@users.noreply.github.com>
2025-12-09 16:02:15 +0330

    clean up

--- components/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AuthForm } from "./AuthForm";
-export { AuthProvider, useAuth } from "../../context/AuthContext";
-export { default as LogoutButton } from "./LogoutButton";
-export { default as UserProfile } from "./UserProfile";
