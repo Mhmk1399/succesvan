628704a233beb35e7f7894ecad421a63341840a4
Mhmk1399 <mhmk1399@gmail.com>
2026-02-03 15:11:05 +0800

    build 2/2

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,27 +66,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="theme-color" content="#fe9a00" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest"></link>
-      </head>
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased custom-scrollbar`}
       >
@@ -94,7 +73,7 @@ export default function RootLayout({
           <AnnouncementBar />
           <Navbar />
           <CanonicalUrl />
-          <Toaster position="bottom-center" />
+          <Toaster position="bottom-center"  />
           <MobileAppPrompt />
 
           {children}
